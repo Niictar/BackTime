4566634e4550cf8edfb880cbe1a642254f6854f0
Archenoth <archenoth@gmail.com>
2015-06-06 15:42:03 -0600

    Initial commit of Retrospective Timesheet
    The idea behind this project is to help analyze usage of a system to try
    to determine when the system was being used, and for what.
    
    The farther back this code goes, the less reliable it will be, but
    hopefully it will be helpful for getting people out of a pinch if they
    need to figure out the amount of time they have been working on a
    particular project.

---
+++ timesheet.rb
@@ -0,0 +1,20 @@
+require_relative 'time'
+
+# Adds a folder to the database's time records.
+#
+# If there is no path specified, it adds the current user's home
+# folder.
+def add_folder(path = Dir.home)
+  Dir.glob "#{path}/**/*" do |filename|
+    stat = File.stat filename
+
+    TimeEntry.create(
+      :name => File.absolute_path(filename),
+      :type => "File",
+      :created => stat.ctime,
+      :modified => stat.mtime,
+      :accessed => stat.atime,
+      :recorded => Time.now
+    ).save
+  end
+end
