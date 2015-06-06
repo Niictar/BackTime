158015b68c9b774252ae19dcecade4d1b54dc75d
Archenoth <archenoth@gmail.com>
2015-06-06 16:28:44 -0600

    Moved reset function, added Firefox history import
    Currently quite slow... But it imports correctly.
    I need to look into a way I can speed this up -- likely with a cache for
    Firefox places and looking into speeding up DataMapper inserts. (Which I
    noticed were not terribly fast either.)

--- timesheet.rb
+++ timesheet.rb
@@ -1,4 +1,10 @@
+require 'sqlite3'
 require_relative 'time'
+
+# Super-dangerous... Resets the database back to nothingness!
+def reset_database
+  DataMapper.auto_migrate!
+end
 
 # Adds a folder to the database's time records.
 #
@@ -20,7 +26,28 @@ def add_folder(path = Dir.home)
   end
 end
 
-# Super-dangerous... Resets the database back to nothingness!
-def reset_database
-  DataMapper.auto_migrate!
+# Adds a Firefox history file to the database's time records.
+#
+# This type of add will set the created, accessed, and modified dates
+# to the date of visiting each site.
+#
+# The Firefox history file is an SQLite database called
+# "places.sqlite" that should be in the root of your Firefox profile.
+def add_firefox_history(history_file)
+  SQLite3::Database.new history_file do |db|
+    db.execute "SELECT * FROM moz_historyvisits" do |history_entry|
+      place = db.execute("SELECT * FROM moz_places where id = ?", history_entry[2]).first
+      visit_time = Time.at(history_entry[3] / 1000000)
+
+      TimeEntry.create(
+        :name => place[3],
+        :path => place[1],
+        :type => "Firefox History",
+        :created => visit_time,
+        :modified => visit_time,
+        :accessed => visit_time,
+        :recorded => Time.now
+      ).save
+    end
+  end
 end
