9e4d63880ef8367ff5aac40fcaa5f5d83d4c70a5
Archenoth <archenoth@gmail.com>
2015-06-07 02:42:37 -0600

    Moved ConfigWindow to its own file, made base Gui class.
    ConfigWindow should also now play nicely with other UI. (For example,
    closing its window should not kill the main Tk runtime.)

---
+++ gui/configWindow.rb
@@ -0,0 +1,59 @@
+class ConfigWindow
+  def initialize(root_window)
+    top = TkToplevel.new(root_window) do
+      resizable false, false
+      title "Retrospective Timesheet setup"
+    end
+
+    selection = TkFrame.new(top) do
+      grid :row => 0, :column => 0
+    end
+
+    TkLabel.new(selection) do
+      text "Database:"
+      height 3
+      padx 5
+      grid :row => 0, :column => 0
+    end
+
+    database = TkText.new(selection) do
+      width 30
+      height 1
+      grid :row => 0, :column => 1
+    end
+
+    TkButton.new(selection) do
+      text "Select"
+      width 7
+      grid :row => 0, :column => 2, :padx => 7
+      command do
+        selected = getOpenFile
+        database.value = selected unless selected.empty?
+      end
+    end
+
+    bottom_frame = TkFrame.new(selection) do
+      pady 5
+      borderwidth 1
+      relief "solid"
+      background "grey"
+      grid :row => 1, :column => 0, :columnspan => 3, :sticky => "ew"
+    end
+
+    TkButton.new(bottom_frame) do
+      text "Ok"
+      width 7
+      pack :side => "right", :padx => 5
+      command do
+        top.destroy
+      end
+    end
+
+    TkButton.new(bottom_frame) do
+      text "Cancel"
+      width 7
+      pack :side => "right"
+      command {top.destroy}
+    end
+  end
+end
