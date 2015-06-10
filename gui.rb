f3282308532d08d318e489c4c26306b282722a90
Archenoth <archenoth@gmail.com>
2015-06-09 20:28:20 -0600

    Fleshed out and moved main Gui logic. (Not appearance)
    Now the Gui class is simply a dispatcher for other GUIs, and will
    contain the logic of when to show and hide them.
    
    MainWindow will be the class that holds onto an instance of the root
    window, so it will be easy to find, and to kill the Tk runtime when the
    window closes.

--- gui.rb
+++ gui.rb
@@ -6,14 +6,16 @@ require_relative 'timesheet'
 class Gui
   attr_reader :root_window
 
+  # Creates a visible Gui instance
+  #
+  # Accepts a single paramater to determine if you want to log to
+  # standard out or not. true for yes, false for no. Default is false.
   def initialize(log = false)
-    @root_window = TkRoot.new do
-      width 500
-      height 500
-    end.withdraw
+    main_window = MainWindow.new true
 
-    ConfigWindow.new @root_window do |database_file|
-      MainWindow.new @root_window, TimeSheet.new(database_file, log)
+    ConfigWindow.new main_window.root_window do |database_file|
+      main_window.timesheet = TimeSheet.new database_file, log
+      main_window.show
     end
 
     Tk.mainloop
--- gui/mainWindow.rb
+++ gui/mainWindow.rb
@@ -1,5 +1,34 @@
 class MainWindow
-  def initialize(root_window, timesheet)
-    root_window.deiconify
+  attr_reader :root_window, :analyzer
+  attr_accessor :timesheet
+
+  # Creates the main TK window
+  # Accepts one argument that allows you to start this window hidden
+  # or not. true for hidden, false otherwise. Default is visible.
+  def initialize(hidden = false)
+    @root_window = TkRoot.new do
+      title "Time Analyzer"
+      width 500
+      height 500
+    end
+
+    hide if hidden
+  end
+
+  # Shows the main window
+  def show
+    @root_window.deiconify
+  end
+
+  # Hides the main window
+  def hide
+    @root_window.withdraw
+  end
+
+  # Assigns the timesheet to the passed in value and creates an
+  # analyzer for it.
+  def timesheet=(timesheet)
+    @timesheet = timesheet
+    @analyzer = TimeAnalyzer.new timesheet
   end
 end
